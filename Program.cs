e8c47e46698f847b2e7d6ffdc0b9c82eb9e02468
jtavora <jtavora09@gmail.com>
2024-11-20 15:47:16 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Teste Nokia:");
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTeste iPhone:");
+        Smartphone iphone = new Iphone("654321", "iPhone 13", "222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
